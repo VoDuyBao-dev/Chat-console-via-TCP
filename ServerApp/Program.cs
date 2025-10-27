086457d73e8dbe413b48bc657ee5e64614d6ee7d
VoThanhTruc24 <vothanhtruc2401@gmail.com>
2025-10-27 21:43:50 +0700

    Push full first code ChatApp

---
+++ ServerApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ServerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "TCP Chat Server";
+
+            Server server = new Server();
+            server.Start(5000); 
+
+            Console.ReadLine();
+        }
+    }
+}
