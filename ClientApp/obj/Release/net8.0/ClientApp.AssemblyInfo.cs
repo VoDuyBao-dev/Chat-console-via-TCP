f9bab5ddeb0a09f8cb78efe245e037a15273e0cb
Tuyet Sang <sanghtt1001@ut.edu.vn>
2025-11-04 22:36:40 +0700

    first commit

--- ClientApp/obj/Release/net8.0/ClientApp.AssemblyInfo.cs
+++ ClientApp/obj/Release/net8.0/ClientApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+086457d73e8dbe413b48bc657ee5e64614d6ee7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
