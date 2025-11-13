fb650a3b1980c4385b82a6671aac4b1b0cc28f7a
MinhChau003 <chau03032005@gmail.com>
2025-11-13 23:52:47 +0700

    Them lop user mieu ta thong tin nguoi dung ben server

--- ServerApp/Models/User.cs
+++ ServerApp/Models/User.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ServerApp.Models
 {
-    internal class User
+    public class User
     {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsOnline { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public DateTime? LastLogout { get; set; }
+
+        public TcpClient Client { get; set; } // ket noi dang dung
+
+        public User() { }
+
+        public User(string username, TcpClient client)
+        {
+            Username = username;
+            Client = client;
+            IsOnline = true;
+        }
     }
 }
